c8cbcc1df32a0496331c3023f1b2a241ba70ef5a
matia-gov <mohamed.atia@phila.gov>
2019-08-28 13:42:03 -0400

    initial commit with phila standard app html

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="example" />
+</template>
+
+<script>
+import Vue from "vue";
+import axios from "axios";
+
+import VuePaginate from "vue-paginate";
+import VueFuse from "vue-fuse";
+
+Vue.use(VuePaginate);
+Vue.use(VueFuse);
+
+const endpoint = "https://data.phila.gov/carto/api/v2/sql?q=";
+
+export default {
+  name: "FindALicensedContractor",
+  components: { 
+    
+  },
+  filters: {
+    
+  },
+  data: function() {
+    return {
+    
+    };
+  },
+  computed: { 
+    
+  },
+
+  watch: {
+   
+  },
+
+  created: {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" >
+.example {
+  display: none;
+}
+
+.appnav {
+  display: none;
+}
+</style>
