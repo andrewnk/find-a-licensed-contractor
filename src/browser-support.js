9651390001b04952163fea5b2af5abad5a027fd8
matia-gov <mohamed.atia@phila.gov>
2019-10-02 14:02:59 -0400

    only load polyfills on ie

---
+++ src/browser-support.js
@@ -0,0 +1,15 @@
+function isIE() {
+  var ua = navigator.userAgent;
+  /* MSIE used to detect old browsers and Trident used to newer ones*/
+  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+  if (is_ie) {
+    require("./polyfills/Array.prototype.find.js");
+    require("./polyfills/Array.prototype.includes.js");
+    require("./polyfills/Promise.prototype.finally.js");
+  }
+}
+
+export default {
+  isIE,
+};
